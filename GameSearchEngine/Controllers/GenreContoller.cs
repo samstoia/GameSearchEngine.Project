401b692c259ade04196b03e9fcc615c4f4c4df1c
samstoia <samstoia@gmail.com>
2019-03-11 22:56:25 -0700

    building out views

--- GameSearchEngine/Controllers/GenreContoller.cs
+++ GameSearchEngine/Controllers/GenreContoller.cs
@@ -11,5 +11,59 @@ namespace GameSearchEngine.Controllers
 		{
 		return View();
 		}
+
+		[HttpGet("/genre/action")]
+		public ActionResult Action()
+		{
+		return View();
+		}
+
+		[HttpGet("/genre/adventure")]
+		public ActionResult Adventure()
+		{
+		return View();
+		}
+
+		[HttpGet("/genre/compilation")]
+		public ActionResult Compilation()
+		{
+		return View();
+		}
+
+		[HttpGet("/genre/puzzle")]
+		public ActionResult Puzzle()
+		{
+		return View();
+		}
+
+		[HttpGet("/genre/racingdriving")]
+		public ActionResult RacingDriving()
+		{
+		return View();
+		}
+		[HttpGet("/genre/roleplaying")]
+		public ActionResult RolePlaying()
+		{
+		return View();
+		}
+		[HttpGet("/genre/simulation")]
+		public ActionResult Simulation()
+		{
+		return View();
+		}
+
+		[HttpGet("/genre/sports")]
+		public ActionResult Sports()
+		{
+		return View();
+		}
+
+		[HttpGet("/genre/strategy")]
+		public ActionResult Strategy()
+		{
+		return View();
+		}
 	}
+
+	
 }
